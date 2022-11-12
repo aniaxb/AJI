27171b7d44fe689a0c90bce5a14a8724ac51c77f
236493 Banachowicz Anna <236493@edu.p.lodz.pl>
2022-11-12 20:26:22 +0100

    Changed form look with bootstrap

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -2,22 +2,31 @@
    <form >
         <div class="form-group">
             <label for="titleInput">Tytuł</label><br>
-        <input type="text" name="" id="titleInput" placeholder="Search a movie">
+            <input class="form-control" type="text" name="" id="titleInput" placeholder="Podaj tytuł lub fragment tytułu filmu">
         </div>
-        <div class="form-group">
-            <label for="startYear">Rok produkcji od:</label>
-        <input type="number" id="startYear" placeholder="Liczba naturalna z przedziału 1900-2019">
+        <div class="form-group row">
+            <div class="col-sm-4">
+                <label for="startYear">Rok produkcji od:</label>
+            </div>
+            <div class="col-sm-8">
+                <input class="form-control" type="number" id="startYear" placeholder="Liczba naturalna z przedziału 1900-2019">
+            </div>
         </div>
-        <div class="form-group">
-            <label for="endYear">Rok produkcji do:</label>
-        <input type="number" id="endYear" placeholder="Liczba naturalna z przedziału 1900-2019">
+
+        <div class="form-group row">
+            <div class="col-sm-4">
+                <label for="endYear">Rok produkcji do:</label>
+            </div>
+            <div class="col-sm-8">
+                <input class="form-control" type="number" id="endYear" placeholder="Liczba naturalna z przedziału 1900-2019">
+            </div>
         </div>
         <div class="form-group">
             <label for="cast">Obsada</label><br>
-        <input type="text" name="" id="cast" placeholder="Imię i nazwisko">
+            <input class="form-control" type="text" name="" id="cast" placeholder="Imię i nazwisko">
         </div>
         <div class="form-group row">
-            <button>Szukaj</button>
+            <button class="btn btn-info col-sm-12">Szukaj</button>
         </div>
    </form>
         
@@ -32,11 +41,5 @@ export default{
 
 <style scoped>
 
-button {
-    /* display: inline-block; */
-    width: 50%;
-    background-color: #138496;
-    color: white;
-    text-align: center;
-}
+
 </style>
